73d71774830a2459b68381b0cd6224693844099e
rabiulahsan <ra.rabiulahsan@gmail.com>
2025-01-18 21:00:42 +0600

    created footer

--- email-template-client/src/Layout/MainLayout.jsx
+++ email-template-client/src/Layout/MainLayout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router";
 import Navbar from "../Components/Navbar/Navbar";
+import Footer from "../Components/Footer/Footer";
 
 const MainLayout = () => {
   return (
     <>
       <Navbar></Navbar>
       <Outlet></Outlet>
+      <Footer></Footer>
     </>
   );
 };
