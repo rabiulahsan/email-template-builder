943fdd1a81b51bb95be52bd53acfa9e172c42098
rabiulahsan <ra.rabiulahsan@gmail.com>
2025-01-18 19:43:21 +0600

    connected to mongodb

--- email-template-server/index.js
+++ email-template-server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const { run } = require("./utils/dbConnection");
 
 require("dotenv").config();
 
@@ -9,6 +10,8 @@ const port = process.env.PORT || 5000;
 //middleware
 app.use(cors());
 app.use(express.json());
+
+run();
 
 //test
 app.get("/", (req, res) => {
