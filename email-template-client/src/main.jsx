dc1b27abcb99e0179f4e1842cc64536ad2529fee
rabiulahsan <ra.rabiulahsan@gmail.com>
2025-01-22 06:50:47 +0600

    remove unnecessary things

--- email-template-client/src/main.jsx
+++ email-template-client/src/main.jsx
@@ -3,19 +3,13 @@ import "./index.css";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { router } from "./Routes/routes";
 const queryClient = new QueryClient();
-import { Provider } from "react-redux";
-import { store } from "./Redux/Store/Store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <Provider store={store}>
-        <RouterProvider router={router}></RouterProvider>
-        <ReactQueryDevtools initialIsOpen={false} />
-      </Provider>
+      <RouterProvider router={router}></RouterProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );
